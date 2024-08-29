d3f3221603619710c2f8197170c3d23b856dc1cb
HwiGyoung Lee <ter0000@naver.com>
2024-08-29 18:05:54 +0900

    � imageslider width height calc 적용

---
+++ src/image.js
@@ -0,0 +1,4 @@
+const slider = document.querySelector(".image-slider");
+const main_img = slider.querySelector('.active');
+const next_btn = slider.querySelector('.btn-next');
+const prev_btn = slider.querySelector('.btn-prev')
